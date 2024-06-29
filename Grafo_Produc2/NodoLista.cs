e911be8cf059d4b08b12a57cf52e61c575796f44
Bruno Humberto Juarez Gomez <utp0150224@alumno.utpuebla.edu.mx>
2024-06-29 14:29:14 -0600

    Creacion de proyecto y repositorio

---
+++ Grafo_Produc2/NodoLista.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grafo_Produc2
+{
+    public class NodoLista
+    {
+        //
+        public int vertexNum = -5;
+        //costo para llegar al vertice
+        public float costs { get;  set; }
+        //Enlace lista ligada
+        public NodoLista next = null;
+    }
+}
---
+++ Grafo_Produc2/Usuario.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grafo_Produc2
+{
+    public class Usuario
+    {
+        public int IdUsuario { get; set; }
+        public string Nombres { get; set; }
+        public string Apellidos { get; set; }
+        public  int Edad{ get; set; }
+
+        public Usuario() { }
+
+        public Usuario(int id2, string nombres, string apellidos,int edad)
+        {
+            IdUsuario = id2;
+            Nombres = nombres;
+            Apellidos = apellidos;
+            Edad = edad;
+        }
+
+
+    }
+}
